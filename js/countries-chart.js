2353082dcfd7cd3bb2a55e64887ada4f6b236bd0
mlodykordian <79373930+mlodykordian@users.noreply.github.com>
2021-03-14 22:37:43 +0100

    Update countries-chart.js

--- js/countries-chart.js
+++ js/countries-chart.js
@@ -20,39 +20,45 @@
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
-    type: 'doughnut',
+    type: 'pie',
     data: {
-    labels: ['Bułgaria', 'Chorwacja', 'Cypr', 'Czechy', 'Estonia', 'Grecja', 'Węgry', 'Łotwa', 'Litwa', 'Malta', 'Polska', 'Portugalia', 'Rumunia', 'Słowacja', 'Słowenia'],
+    labels: ['Polska', 'Rumunia', 'Bułgaria', 'Węgry', 'Czechy', 'Litwa', 'Grecja', 'Słowacja', 'Chorwacja', 'Portugalia', 'Łotwa', 'Estonia', 'Słowenia', 'Cypr', 'Malta'],
     datasets: [{
             label: '% funduszy',
-            data: [7.1, 3.5, 0.4, 6.2, 2.3, 3.9, 6.9, 3.4, 4, 0.3, 27.3, 3.5, 26.2, 3.8, 1.3],
+            data: [27.3, 26.2, 7.1, 6.9, 6.2, 4.0, 3.9, 3.8, 3.5, 3.5, 3.4, 2.3, 1.3, 0.4, 0.3],
             backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 206, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(153, 102, 255)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 206, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(153, 102, 255)',
-                'rgb(255, 159, 64)'
+                '#ae1515', //1
+                '#be3535', //2
+                '#ce5555', //3
+                '#de7575', //4
+                '#ee9595', //5
+                '#f4aa06', //6
+                '#f4ba26', //7
+                '#f4ca46', //8
+                '#f4da66', //9
+                '#f4ea86', //10
+                '#c4a5f2', //11
+                '#c4b5f2', //12
+                '#c4c5f2',//13
+                '#c4d5f2',//14
+                '#c4e5f2'//15
             ],
             borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 206, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(153, 102, 255)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 206, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(153, 102, 255)',
-                'rgb(255, 159, 64)'
+                '#ae1515', //1
+                '#be3535', //2
+                '#ce5555', //3
+                '#de7575', //4
+                '#ee9595', //5
+                '#f4aa06', //6
+                '#f4ba26', //7
+                '#f4ca46', //8
+                '#f4da66', //9
+                '#f4ea86', //10
+                '#c4a5f2', //11
+                '#c4b5f2', //12
+                '#c4c5f2',//13
+                '#c4d5f2',//14
+                '#c4e5f2'//15
             ],
             borderWidth: 1
         }]
